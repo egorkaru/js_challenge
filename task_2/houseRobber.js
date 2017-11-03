8159f9c56888eb5be231d3d4a0fd046ac5386dcc
Egor Rudinsky <rudgv2008@gmail.com>
2017-11-03 10:07:53 +0300

    сделал houseRobber

--- task_2/houseRobber.js
+++ task_2/houseRobber.js
@@ -1,3 +1,15 @@
 function houseRobber(nums) {
-
+    const length = nums.length
+    if (length == 0)
+        return 0
+    if (length == 1)
+        return nums[0]
+    const result = [
+        nums[0], 
+        Math.max(nums[0], nums[1])
+    ]
+    for(let i = 2; i < length; i++){
+        result.push(Math.max(nums[i] + result[i - 2], result[i - 1]))
+    }
+    return result[length - 1]
 }
